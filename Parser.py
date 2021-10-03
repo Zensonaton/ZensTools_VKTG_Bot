2aad0efef5f322961f0ed74f26a9ad40c6d6d5fa
Zensonaton <zensonaton@gmail.com>
2021-10-03 22:29:00 +0600

    Перенос ключей (и не только) в папку "BLKeys".

--- Parser.py
+++ Parser.py
@@ -9,7 +9,7 @@ def parse_decoded_text(plain_text: str) -> str:
 	pass
 
 def decrypt_plain(encrypted_text: str) -> str:
-	"""Дешифровывает plain-файл, зашифрованный PGP-Ключем (который хранится в файле `BL-Key`) с Secret phrase (в файле `BL-SecretPhrase`) и выдаёт дешифрованный результат. Код у данной функции немного кривой, ибо я понятия не имею как *должным* образом работать с библиотекой pgpy.
+	"""Дешифровывает plain-файл, зашифрованный PGP-Ключем (который хранится в файле `BLKeys/Private key`) с Secret phrase (в файле `BLKeys/Secret phrase`) и выдаёт дешифрованный результат. Код у данной функции немного кривой, ибо я понятия не имею как *должным* образом работать с библиотекой pgpy.
 
 	Args:
 		encrypted_text (str): Зашифрованный текст, который должен быть дешифрован PGP-Ключём. (Не путь к файлу, а его содержимое!)
@@ -19,7 +19,7 @@ def decrypt_plain(encrypted_text: str) -> str:
 	"""
 
 	encrypted_text 	= pgpy.PGPMessage.from_blob(encrypted_text)
-	key, _			= pgpy.PGPKey.from_file("BL-Key")
+	key, _			= pgpy.PGPKey.from_file("BLKeys/Private key")
 
-	with key.unlock( open("BL-SecretPhrase", "r", encoding = "utf-8").read() ):
+	with key.unlock( open("BLKeys/Secret phrase", "r", encoding = "utf-8").read() ):
 		return key.decrypt(encrypted_text).message.decode("utf-8")
