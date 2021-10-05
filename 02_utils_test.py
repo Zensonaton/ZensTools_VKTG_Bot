8ee91175c7b51cac489b3d9244b878ba3bf0df71
Zensonaton <zensonaton@gmail.com>
2021-10-05 13:38:46 +0600

    Система хранения данных юзеров.

--- 02_utils_test.py
+++ 02_utils_test.py
@@ -1,13 +1,30 @@
 # coding: utf-8
 
-import pytest
-
-from Utils import is_a_valid_index_url
-import pgpy
-import json
+import Utils as utils
+import shutil
 
 def test_indexjson_url_checker():
-	assert is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/72e41ed08452870a27fb545006e27781/index.json") # Should be True
-	assert is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/18772e600545bf72a07825480de14e27/index.json") # Should be True
-	assert not is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/THIS_SHOULD_BE_FALSE!/index.json")        # Should be False
-	assert not is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/THIS_SHOULD_BE_FALSE!/index.json.")       # Should be False
+	assert utils.is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/72e41ed08452870a27fb545006e27781/index.json") # Should be True
+	assert utils.is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/18772e600545bf72a07825480de14e27/index.json") # Should be True
+	assert not utils.is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/THIS_SHOULD_BE_FALSE!/index.json")        # Should be False
+	assert not utils.is_a_valid_index_url("https://onlinemektep.net/upload/online_mektep/lesson/THIS_SHOULD_BE_FALSE!/index.json.")       # Should be False
+
+def test_save_db():
+	utils.save_user_info({
+		"Boolean": True
+	}, 1, "Temp-Testing/")
+
+	utils.save_user_info({
+		"Boolean": True
+	}, -1, "Temp-Testing/")
+
+	assert True
+
+def test_load_db():
+	data = utils.load_user_info(-1, "Temp-Testing/")
+
+	assert data["Boolean"]
+
+	shutil.rmtree(
+		"Temp-Testing/"
+	)
