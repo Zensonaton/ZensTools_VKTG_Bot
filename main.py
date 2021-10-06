a4d7c464c661571a92b243db33353d64ebfacf45
Zensonaton <zensonaton@gmail.com>
2021-10-06 18:56:27 +0600

    Жую и ебусь с CSS в одно и то же время! Multitasking!

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # coding: utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.template_folder 		= "www"
+app.static_folder 			= "www"
+app.templates_auto_reload 	= True
+
 @app.route("/")
 def main():
-	return "Main page"
+	return render_template("main.html")
 
 if __name__ == "__main__":
 	app.run()
